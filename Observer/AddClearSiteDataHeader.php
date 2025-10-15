b4ce751dcab294ede9a86aadd62acaf6e268ab4e
Finn Reinhardt <f.reinhardt@basecom.de>
2025-10-15 16:24:05 +0200

    Add Observer to add the Clear-Site-Data header after idempotent cart action

---
+++ Observer/AddClearSiteDataHeader.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Basecom\Catalog\Observer;
+
+use Magento\Framework\App\Response\HttpInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+/**
+ * Stale prefetches can be cleared using the `prefetchCache` and `prerenderCache` value of the `Clear-Site-Data response header.
+ * See [MDN documentation](https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Clear-Site-Data#prefetchcache)
+ */
+class AddClearSiteDataHeader implements ObserverInterface
+{
+    public function execute(Observer $observer): void
+    {
+        /** @var HttpInterface|null $response */
+        $response = $observer->getResponse();
+
+        if (!$response) {
+            return;
+        }
+
+        $response->setHeader(
+            'Clear-Site-Data',
+            '"prefetchCache", "prerenderCache"',
+            true
+        );
+    }
+}
